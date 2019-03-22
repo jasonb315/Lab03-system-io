66eea58013dde8898e012b0a50de4c02235d43c8
Jason Burns <jasonb315@protonmail.com>
2019-03-22 11:12:02 -0700

    read in file

--- WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -47,7 +47,16 @@ namespace WordGuessGame
                 }
             }
 
-            // if file, load
+            // read in file:
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
         }
         static void Play()
         {
