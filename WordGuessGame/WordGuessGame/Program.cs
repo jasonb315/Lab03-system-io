cd8656885429f15da398ffd29c0c31dc1c88d9d9
Jason Burns <jasonb315@protonmail.com>
2019-03-22 10:45:17 -0700

    scaffold

--- WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -6,7 +6,52 @@ namespace WordGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                FileSetup();
+                Home();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
+        static void Home()
+        {
+            // intro screen
+            // user interface/selection
+                // CRUD words to file
+        }
+        static void FileSetup()
+        {
+            // check for file
+            // if no file, create
+            // if file, load
+        }
+        static void Play()
+        {
+            // load words from file and select random
+            // while true loop guessing
+            // track guesses
+            // completion event!!
+            // play again ? play : home/exit
+        }
+        static void ViewWords()
+        {
+            // io
+        }
+        static void AddWords()
+        {
+            // io
+        }
+        static void RemoveWord()
+        {
+            // io
         }
     }
 }
