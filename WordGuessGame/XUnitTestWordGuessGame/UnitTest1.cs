a8d47e4cc92979c221857c6d8908f2c6ee2071bb
Jason Burns <jasonb315@protonmail.com>
2019-03-22 20:15:57 -0700

    testing

--- WordGuessGame/XUnitTestWordGuessGame/UnitTest1.cs
+++ WordGuessGame/XUnitTestWordGuessGame/UnitTest1.cs
@@ -1,14 +1,48 @@
 using System;
+using System.IO;
 using Xunit;
 
-namespace XUnitTestWordGuessGame
+namespace WordGuessGame
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FileCreation()
         {
+            string path = "../../../../WordGuessGame/wordBank.txt";
 
+            bool fileExists = File.Exists(path);
+
+            Assert.True(fileExists);
+        }
+
+        [Fact]
+        public void FileRead()
+        {
+            string[] words = new string[11];
+
+            string path = "../../../../WordGuessGame/wordBank.txt";
+            int i = 0;
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    words[i] = s;
+                    i++;
+                }
+            }
+            int wordArrLen = words.Length;
+            Assert.Equal(11, wordArrLen);
+        }
+
+        [Fact]
+        public void LetterRead()
+        {
+            string word = "dog";
+            bool wordHasLetter = word.Contains("o");
+            Assert.True(wordHasLetter);
         }
     }
 }
